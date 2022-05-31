c2c054e7dbe832ee65780d0c44571b7fb558d245
Dmitrii <doronin_dima@mail.ru>
2022-05-31 18:44:27 +0300

    put

--- AJAX/service.js
+++ AJAX/service.js
@@ -12,11 +12,41 @@ function getTasks() {
   })
 }
 
-function createTask(title) {
-  const promise = axios.post(`https://repetitora.net/api/JS/Tasks?widgetId=123&title=${title}`)
-  return promise.then((response) => {
-    return response.data
-  })
-}
+// function createTask(title) {
+//   const promise = axios.post(`https://repetitora.net/api/JS/Tasks?widgetId=123&title=${title}`)
+//   return promise.then((response) => {
+//     return response.data
+//   })
+// }
 
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+
+  fetch('https://jsonplaceholder.typicode.com/posts/1', {
+    method: 'PUT',
+    body: JSON.stringify({
+      id: 1,
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
+      name: 'Dima'
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
 
